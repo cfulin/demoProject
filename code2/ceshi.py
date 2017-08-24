43be1d17bd82969081c8c6edac46c458d04e76ad
cfulin <740494415@qq.com>
2017-08-24 14:07:09 +0800

    # study

---
+++ code2/ceshi.py
@@ -0,0 +1,59 @@
+from __future__ import division
+import numpy as np
+
+
+# print 1/2
+# name = raw_input("input your name: ")
+# print 'hello' + " " + name
+# raw_input("Press <enter>")
+# name = list('Perl')
+# name[2:] = list('aa')
+# number = [1, 5]
+# number[1:1] = [2, 3, 4]
+# number.insert(3, 'aa')
+# number.reverse()
+# cmp(10, 10)
+# number.sort(cmp)
+# print 'PI: %-10.5s' % np.pi
+#
+# from string import maketrans
+#
+# table = maketrans('cs', 'kz')
+# print "nadn  jaknkj  vjsdnjkn clllks".translate(table, " ")
+
+
+# people = {
+#     'Alice': {
+#         'phone': '123',
+#         'addr': 'awqcvdbc'
+#     },
+#
+#     'Bob': {
+#         'phone': '213',
+#         'addr': 'asdaadac'
+#     },
+#
+#     'Jim': {
+#         'phone': '231',
+#         'addr': 'afdabfbc'
+#     },
+#
+# }
+#
+# labels = {
+#     'phone': 'Phone Number',
+#     'addr': 'address'
+# }
+#
+# name = raw_input('Name: ')
+#
+# request = raw_input('Phone number(p) or address(a)? ')
+#
+# if request == 'p':
+#     key = 'phone'
+# if request == 'a':
+#     key = 'addr'
+#
+# if name in people:
+#     print "%s's %s is %s." % (name, labels[key], people[name][key])
+
